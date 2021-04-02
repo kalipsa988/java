2ccecbaa3a424c60ce44d8e11b66c1fd4f43963d
Роман <kalipsa988@mail.ru>
2021-04-02 19:34:11 +0300

    Update Main.java

--- 4/src/main/java/Main.java
+++ 4/src/main/java/Main.java
@@ -7,20 +7,35 @@ public class Main {
 
         int i;
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите размер матрицы ");
+        System.out.print("Введите число размер матрицы ");
         int x = in.nextInt();
         int y = in.nextInt();
 
-        boolean [][]mas = new boolean[x][y];
+        int [][]mas = new int[x][y];
 
+        int max = 50;
+        int min = -50;
         Random random = new Random();
 
         for(i = 0; i < x; i++) {
             for (int k=0; k<y;k++){
-                mas[i][k] = true;//random.nextBoolean();
+                mas[i][k] = random.nextInt(50) -50;
                 System.out.print(mas[i][k] + " ");
             }
             System.out.println(" ");
         }
+        System.out.print("Введите диапазон: ");
+        int minDip = in.nextInt();
+        int maxDip = in.nextInt();
+
+        for(i = 0; i < x; i++) {
+            for (int k=0; k<y;k++){
+                if((mas[i][k] > minDip &&(mas[i][k] < maxDip))){
+                    System.out.println("True");
+                }
+                else System.out.println("False");
+            }
+        }
+
     }
 }
