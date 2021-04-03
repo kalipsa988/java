ba5159480c79281d6d73212a3c07c2d785b58da0
Роман <kalipsa988@mail.ru>
2021-04-03 09:41:54 +0300

    Update Main.java

--- 4/src/main/java/Main.java
+++ 4/src/main/java/Main.java
@@ -39,3 +39,42 @@ public class Main {
 
     }
 }
+
+import static java.lang.Math.*;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i;
+        int lastPosition = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число размер матрицы ");
+        int x = in.nextInt();
+
+        int []mas = new int[x];
+
+        Random random = new Random();
+
+        for(i = 0; i < x; i++) {
+                mas[i] = random.nextInt(10) ;
+                System.out.print(mas[i] + " ");
+        }
+        System.out.println(" ");
+
+        for(i=0; i<mas.length; i++) {
+                if(mas[i]!= 0) {
+                    mas[lastPosition++] = mas[i];
+                }
+        }
+
+        for( i=lastPosition; i<mas.length; i++) {
+            mas[i]=0;
+        }
+
+        for(i = 0; i < x; i++) {
+            System.out.print(mas[i] + " ");
+        }
+    }
+}
