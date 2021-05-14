bbe2ec0d01352ec9b07d89d02d564cfe278e74ef
Роман <kalipsa988@mail.ru>
2021-05-14 23:28:05 +0300

    Update Main.java

--- 5/src/main/java/Main.java
+++ 5/src/main/java/Main.java
@@ -5,127 +5,49 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int i = 0;
+        int i;
+        int z;
+        int j;
+        int y;
+        int lastPosition = 0;
+        int count=0;
+
         Scanner in = new Scanner(System.in);
         System.out.print("Введите число размер матрицы ");
         int x = in.nextInt();
 
         int []mas = new int[x];
 
-        int max = 10;
-        int min = 0;
         Random random = new Random();
 
         for(i = 0; i < x; i++) {
-            mas[i] = random.nextInt(10);
+            mas[i] = random.nextInt(5);
+            System.out.print(mas[i] + " ");
+        }
+        System.out.println(" ");
+
+        for(i=0; i<mas.length; i++) {
+            if(mas[i]!= 0) {
+                mas[lastPosition++] = mas[i];
+            }else count++;
         }
 
-        int lastPosition = 0;
-        for(i = 0; i<mas.length; i++) {
-            if(mas[i] != 0) {
-                mas[lastPosition++] = mas[i];
-                System.out.print(mas[i] + " ");
-                System.out.println(" ");
+        for( i=lastPosition; i<mas.length; i++) {
+            mas[i]=0;
+        }
+
+        if(count!=0) {
+
+            count--;
+            for(j=0; j<=count; j++) {
+                y=x-1;
+                for(i=y; i>=1; i--) {
+                    z=mas[i-1];
+                    mas[i] = z;
+                }
+                mas[0]=0;
             }
         }
-        for(i=lastPosition; i<mas.length; i++) {
-            mas[i]=0;
-            System.out.print(mas[i] + " ");
-            System.out.println(" ");
-        }
-    }
-}
-
-import static java.lang.Math.*;
-import java.util.Random;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-
-        int i;
-        int lastPosition = 0;
-        int count=0;
-
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите число размер матрицы ");
-        int x = in.nextInt();
-
-        int []mas = new int[x];
-
-        Random random = new Random();
-
-        for(i = 0; i < x; i++) {
-            mas[i] = random.nextInt(10);
-            System.out.print(mas[i] + " ");
-        }
-        System.out.println(" ");
-
-        for(i=0; i<mas.length; i++) {
-            if(mas[i]!= 0) {
-                mas[lastPosition++] = mas[i];
-            }else count++;
-        }
-        System.out.println(count);
-        for( i=lastPosition; i<mas.length; i++) {
-            mas[i]=0;
-        }
-        lastPosition = 0;
-        for(i=count; i<mas.length; i++) {
-            mas[i] = mas[lastPosition++];
-        }
-        for(i=0; i<count; i++) {
-            mas[i] = 0;
-        }
-
-        for(i = 0; i < x; i++) {
-            System.out.print(mas[i] + " ");
-        }
-    }
-}
-
-import static java.lang.Math.*;
-import java.util.Random;
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-
-        int i;
-        int lastPosition = 0;
-        int count=0;
-
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите число размер матрицы ");
-        int x = in.nextInt();
-
-        int []mas = new int[x];
-
-        Random random = new Random();
-
-        for(i = 0; i < x; i++) {
-            mas[i] = random.nextInt(10);
-            System.out.print(mas[i] + " ");
-        }
-        System.out.println(" ");
-
-        for(i=0; i<mas.length; i++) {
-            if(mas[i]!= 0) {
-                mas[lastPosition++] = mas[i];
-            }else count++;
-        }
-        System.out.println(count);
-        for( i=lastPosition; i<mas.length; i++) {
-            mas[i]=0;
-        }
-        lastPosition = 0;
-        for(i=count; i<mas.length; i++) {
-            mas[i] = mas[lastPosition++];
-        }
-        for(i=0; i<count; i++) {
-            mas[i] = 0;
-        }
-
         for(i = 0; i < x; i++) {
             System.out.print(mas[i] + " ");
         }
